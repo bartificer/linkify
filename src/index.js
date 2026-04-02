91c79477a92b07bc8b9fa28df0e6cbb761aab8c7
Bart Busschots <opensource@bartificer.net>
2026-04-03 00:40:24 +0100

    Feat: add export of initialised Linkifier as linkify

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import { LinkData } from "./LinkData.class.mjs";
 import { LinkTemplate } from "./LinkTemplate.class.mjs";
 import { PageData } from "./PageData.class.mjs";
 
-// Webpack replaces this line with the actual version string during build
-const VERSION = process.env.VERSION;
-
 // TO DO — more intelligently handle default transformers, ATM they're hard-coded in the Linkifier constructor!!!
 
 // export the public API
-export { Linkifier, PageData, LinkData, LinkTemplate, VERSION};
-export { Linkifier as default };
+const VERSION = process.env.VERSION; // Webpack replaces this line with the actual version string during build
+const linkify = new Linkifier();
+export { linkify, Linkifier, PageData, LinkData, LinkTemplate, VERSION};
+export { linkify as default };
