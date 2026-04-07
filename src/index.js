f50750319579b8d949ffe30abd58b6366e1c0eb8
Bart Busschots <opensource@bartificer.net>
2026-04-07 01:54:31 +0100

    WIP: add basic documentation infrastructure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+/**
+ * The package entry-point.
+ * 
+ * @module linkify
+ * @requires Linkifier
+ * @requires LinkData
+ * @requires LinkTemplate
+ * @requires PageData
+ */
 import { Linkifier } from "./Linkifier.class.mjs";
 import { LinkData } from "./LinkData.class.mjs";
 import { LinkTemplate } from "./LinkTemplate.class.mjs";
 import { PageData } from "./PageData.class.mjs";
 
-// TO DO — more intelligently handle default transformers, ATM they're hard-coded in the Linkifier constructor!!!
+//
+// === export the public API ===
+//
 
-// export the public API
-const VERSION = process.env.VERSION; // Webpack replaces this line with the actual version string during build
-const linkify = new Linkifier();
-export { linkify, Linkifier, PageData, LinkData, LinkTemplate, VERSION};
+/**
+ * The module's current SEMVER version number.
+ * @type {string}
+ */
+export const VERSION = process.env.VERSION; // Webpack replaces this line with the actual version string during build
+
+/**
+ * An instantiated Linkifier object ready for use.
+ * @type {module:Linkifier~Linkifier}
+ */
+export const linkify = new Linkifier();
 export { linkify as default };
+
+export { Linkifier, PageData, LinkData, LinkTemplate};
