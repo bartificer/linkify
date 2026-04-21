c498f94edb7ee29660593f9f2030d63d83dd0b4a
Bart Busschots <opensource@bartificer.net>
2026-04-21 01:33:23 +0100

    WIP: continued work on CLI

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -9,14 +9,13 @@
 /**
  * A commandline inteface (CLI) published as `linkify`.
  * 
- * The CLI can be customised with a module that exports a configuration object as it's default export.
+ * The CLI can be customised with a module that exports a configured `Linkifier` object as it's default export.
  * @module cli
  * @requires node:os
  * @requires node:path
  * @requires node:fs/promise
  * @requires module:commander
- * @requires linkify
- * @see {@link cliConfigObject} for details the configuration object that can be used to customise the CLI.
+ * @requires module:linkifier.Linkifier
  */
 
 //
@@ -33,8 +32,8 @@ import fs from 'node:fs/promises';
 // import Commander.js (CLI framework)
 import { Command } from 'commander';
 
-// import the Linkify — resolves because the name here matches the package name in ../package.json
-import { linkify } from '@bartificer/linkify'
+// import the Linkifier class — resolves because the name here matches the package name in ../package.json
+import { Linkifier } from '@bartificer/linkify'
 
 // --- Conditional Dynamic Imports ---
 
@@ -46,46 +45,75 @@ import { linkify } from '@bartificer/linkify'
 const CONFIG_FILE_NAME = `.linkify-config.mjs`;
 
 /**
- * A function to try load a configuration file. If no path is passed, defaults to `.linkify-config.mjs` in the user's home directory. The value passed will be coerced to a string with `String()`.
- * @param {string} [configPath] — an optional path to load the configuration from.
- * @returns {Object} If no config is found, an empty object is returned.
- * @throws {Error} An Error is thrown if an explicit path is passed and read access is denied, or the file fails to import.
+ * A function to try import a customised Linkfifier object and/or CLI options from a configuration module. The module should
+ * export a single object by default. The exported object should define one or both of the keys `linkifier` or `options`.
+ * `linkifier` should be an instance of the `Linkifier` class, and `options` an object.
+ * 
+ * If no path is passed, will try import from a file named `.linkify-config.mjs` in the user's home directory.
+ * 
+ * If a path is passed, it will be coerced to a string with `String()`.
+ * @param {string} [configPath] — an optional path to import the configuration from.
+ * @returns {cliConfigurationObject} if no path is passed, and there is no module in the default location, an empty object will be returned.
+ * @throws {TypeError} A TypeError is thrown if the loaded module contains a key named `linfifier` that is not an instance of the `Linkifier` class, or, a key named `options` that is not an object.
+ * @throws {Error} An Error is thrown if a path is passed but a module can't be imported from it.
  * @private
  */
-async function loadConfig(configPath = ''){
+async function importConfig(configPath = ''){
     // determine the file path to try load from
     configPath = String(configPath); // coerce the path to a string
-    const fallingBackToDefault = configPath === '' ? false : true;
+    const fallingBackToDefault = configPath === '';
     if(fallingBackToDefault){
         configPath = path.join(os.homedir(), CONFIG_FILE_NAME);
+        console.debug(`no config path specified, falling back to standard: ${configPath}`);
     }
 
     // make sure the path exists
     try{
-        console.debug(`Checking for readable config file: ${configPath}`);
+        console.debug(`checking read access to configuartion module path: ${configPath}`);
         await fs.access(configPath, fs.constants.R_OK);
     } catch (err){
         // only we're only trying as a fallback, don't throw an error, just return an empty object
         if(fallingBackToDefault){
-            console.debug(`Cannot read config file from standatrd location (using defaults): ${configPath}`);
-            return {};
+            console.debug('failed read configuration module, returning default Linkifier and empty option list');
+            return {
+                linkifier: new Linkifier(),
+                options: {}
+            };
         }
 
         // a specific path was passed but it does not exist, throw an error
-        throw new Error('Config not readable', { cause: err });
+        throw new Error('Configuration module not readable', { cause: err });
     }
 
     // try load the config
-    let config = {};
+    let rawConfig = null;
     try {
-        console.debug(`Attempting to load config from: ${configPath}`);
-        config = (await import(configPath)).default;
-        console.debug('OK — config loaded');
+        console.debug(`Attempting to import configuration module from: ${configPath}`);
+        rawConfig = (await import(configPath)).default;
+        console.debug('OK — config imported');
     } catch (err) {
-        throw new Error('Failed to load config', { cause: err });
+        throw new Error('Failed to import configuration module', { cause: err });
     }
 
-    // return the loaded config
+    // extract and verify the expected information
+    const config = {
+        linkifier: null,
+        options: {}
+    };
+    if(rawConfig.hasOwnProperty('linkifier')){
+        if(rawConfig.linkifier instanceof Linkifier){
+            config.linkifier = rawConfig.linkifier;
+        } else {
+            throw new TypeError("Config key 'linkifier' must be an instance of the Linkifier class");
+        }
+    } else {
+        config.linkifier = new Linkifier();
+    }
+    if(rawConfig.hasOwnProperty('options')){
+        config.options = { ...rawConfig.options };
+    }
+
+    // return the imported configuration
     return config;
 }
 
@@ -96,9 +124,9 @@ async function loadConfig(configPath = ''){
 /**
  * The loaded config.
  * @private
- * @type {cliConfigObject}
+ * @type {cliConfigurationObject}
  */
-let config = {};
+let CONFIG = null;
 
 /**
  * The commander object representing the CLI itself.
@@ -118,22 +146,21 @@ generate.hook('preAction', async (cmd) => {
     // capture the passed config path, if any
     const configPath = cli.opts().config || '';
 
-    // try load a config
-    let rawConfig = {};
+    // try import a config
     try {
-        rawConfig = await loadConfig(configPath);
+        CONFIG = await importConfig(configPath);
     } catch (err) {
       console.error(`FATAL: ${err.message}`);
       process.exit(1);
     }
 });
 generate.action(async () => {
-    console.log(await linkify.generateLink('https://www.lets-talk.ie/'));
+    console.log(await CONFIG.linkifier.generateLink('https://www.lets-talk.ie/'));
 });
 
 // execute the CLI
 try {
-    cli.parse();
+    cli.parse(process.argv);
 } catch (err) {
     console.error(`FATAL: ${err.message}`);
     process.exit(1);
