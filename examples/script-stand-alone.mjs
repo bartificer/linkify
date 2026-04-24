9a45bd8e6aa00e677efdb49907a3b34b36e6e922
Bart Busschots <opensource@bartificer.net>
2026-04-24 01:05:17 +0100

    WIP: made a start on stand-alone example script

---
+++ examples/script-stand-alone.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+// Example Linkifier Script - Stand-alone Customisations
+// =====================================================
+// This example script imports a standard linkifier, customises it's link
+// generation process, and then uses it to render links.
+
+// import a default linkifier object and the classes needed to customis it
+import { linkify, LinkData, LinkTemplate } from '@bartificer/linkify';
+
+// enable debug messages for a better understanding of how the module works
+linkify.util.enableDebugMessages();
+
+// because the Linkify API is promise-based, wrap the script logic in a self-executing anonymous async function
+(async () => {
+    // TO DO — add examples from README here
+})();
